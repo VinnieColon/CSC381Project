1c6e611827153bbdf385d564e01ff4ce57c403da
ibr-ai <ibrahima4@lasalle.edu>
2023-10-25 01:08:30 -0400

    Defined file inout form

---
+++ ChooseCSVForm.py
@@ -0,0 +1,11 @@
+# This file contains the code defining the form we will use to have a user enter file into app
+
+
+# Imports
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+
+# Class defining the form
+class FileInForm(FlaskForm):
+    inFile = FileField(validators=[FileRequired(), FileAllowed('csv', 'csv files only')])
