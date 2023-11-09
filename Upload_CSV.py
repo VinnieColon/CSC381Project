073b6ccbe3aeaa9c60bd26cec2193fd99f674370
ibr-ai <ibrahima4@lasalle.edu>
2023-11-08 21:20:26 -0500

    Created page to upload csv

---
+++ Upload_CSV.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+from view.ViewData import view_data
+
+st.title("Upload a CSV")
+
+up_file = st.file_uploader("Select a file")
+
+if up_file is not None:
+    df = pd.read_csv(up_file)
+    df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+    st.session_state["csv_data"] = df
+    view_data()
