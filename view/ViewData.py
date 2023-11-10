ba21e427bc2143d219a0552c6f50ab30fea43961
ibr-ai <ibrahima4@lasalle.edu>
2023-11-09 23:07:20 -0500

    view_data can be told which df to display

--- view/ViewData.py
+++ view/ViewData.py
@@ -13,9 +13,6 @@ def view_data(givenChoiceDF = None):
             df = st.session_state["csv_data"].copy()["0"]
         st.dataframe(df)
 
-        selectDF = st.selectbox("Select Dataframe to View", st.session_state["csv_indexes"].copy().keys())
-        submitDF = st.button("Submit", on_click=view_data, args=selectDF)
-
     else:
         st.subheader("No CSV data entered yet")
 
