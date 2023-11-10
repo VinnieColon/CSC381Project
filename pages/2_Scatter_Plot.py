808f39b9d6d987ce7be055e3daad98cd3227a10d
ibr-ai <ibrahima4@lasalle.edu>
2023-11-09 23:05:20 -0500

    User can choose from saved dfs to plot

--- pages/2_Scatter_Plot.py
+++ pages/2_Scatter_Plot.py
@@ -4,32 +4,33 @@ from matplotlib.figure import Figure
 st.title("Scatter Plot")
 
 if "csv_data" in st.session_state:
-    df = st.session_state["csv_data"].copy()
     fig = Figure()
     enableDownloads = False
-    with st.form("scatter_plot_form"):
 
-        # Defining fields of form
-        st.subheader("Pick Columns to be Plotted")
-        xaxis = st.selectbox("Choose x-axis", df.select_dtypes('floating').columns)
-        yaxis = st.selectbox("Choose y-axis", df.select_dtypes('floating').columns)
-        submitted = st.form_submit_button()
+    # Defining fields of form
+    st.subheader("Choose dataframe")
+    selectDF = st.selectbox("Select a dataframe", st.session_state["csv_indexes"].copy().keys())
+    df = st.session_state["csv_data"].copy()[str(st.session_state["csv_indexes"][selectDF])]
+    st.subheader("Pick Columns to be Plotted")
+    xaxis = st.selectbox("Choose x-axis", df.select_dtypes('floating').columns)
+    yaxis = st.selectbox("Choose y-axis", df.select_dtypes('floating').columns)
+    submitted = st.button("Submit")
 
-        # Defining behavior upon submit
-        if submitted:
+    # Defining behavior upon submit
+    if submitted:
 
-            # Start rendering the downloads button
-            enableDownloads = True
+        # Start rendering the downloads button
+        enableDownloads = True
 
-            # Defining matplotlib figure to be plotted
-            plt = fig.subplots()
-            plt.set_xlabel(xaxis)
-            plt.set_ylabel(yaxis)
-            plt.set_title("{} vs. {}".format(xaxis, yaxis))
-            plt.scatter(df[xaxis], df[yaxis])
+        # Defining matplotlib figure to be plotted
+        plt = fig.subplots()
+        plt.set_xlabel(xaxis)
+        plt.set_ylabel(yaxis)
+        plt.set_title("{} vs. {}".format(xaxis, yaxis))
+        plt.scatter(df[xaxis], df[yaxis])
 
-            # Plotting the figure
-            st.pyplot(fig)
+        # Plotting the figure
+        st.pyplot(fig)
 
     # Button to download figure as png
     fig.savefig('downloads/dwn_fig.png', format='png')
