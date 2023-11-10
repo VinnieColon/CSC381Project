c7bf94b08ae762bbb7cb378441b710aa8435fb54
ibr-ai <ibrahima4@lasalle.edu>
2023-11-09 21:10:16 -0500

    Multiple versions of CSV can be stored

--- Upload_CSV.py
+++ Upload_CSV.py
@@ -9,7 +9,8 @@ up_file = st.file_uploader("Select a file")
 if up_file is not None:
     df = pd.read_csv(up_file)
     df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
-    st.session_state["csv_data"] = df
+    st.session_state["csv_data"] = {"0": df}
+    st.session_state["csv_indexes"] = {"OG data": 0}
     view_data()
 elif "csv_data" in st.session_state:
     view_data()
--- view/ViewData.py
+++ view/ViewData.py
@@ -2,13 +2,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-def view_data():
+def view_data(givenChoiceDF = None):
 
     st.subheader("Data Table")
 
     if "csv_data" in st.session_state:
-        df = st.session_state["csv_data"].copy()
+        if givenChoiceDF is not None:
+            df = st.session_state["csv_data"].copy()[str(st.session_state["csv_indexes"].copy()[givenChoiceDF])]
+        else:
+            df = st.session_state["csv_data"].copy()["0"]
         st.dataframe(df)
+
+        selectDF = st.selectbox("Select Dataframe to View", st.session_state["csv_indexes"].copy().keys())
+        submitDF = st.button("Submit", on_click=view_data, args=selectDF)
+
     else:
         st.subheader("No CSV data entered yet")
 
